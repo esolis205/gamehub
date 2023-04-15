7913bd68f7765add27ad1ba822bd4177fe60f7a7
Enrique <enrique@diontraining.com>
2023-04-15 11:11:58 -0700

    games fetched

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,6 +1,7 @@
 import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <HStack>
       <Switch
